565b834050f8b1a81775eecdade8f2f716a5db3e
cleantomcd <cleanto.santos.macedo.neto@ccc.ufcg.edu.br>
2025-07-07 15:47:37 -0300

    add: PrestacaoContaController.java

---
+++ src/main/java/webcrud/org/control/PrestacaoContaController.java
@@ -0,0 +1,10 @@
+package webcrud.org.control;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+@ApplicationScoped
+public class PrestacaoContaController {
+
+}
