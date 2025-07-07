6dbe4553c6fa96b0dec8d84e7b098108f3bfd1b8
cleantomcd <cleanto.santos.macedo.neto@ccc.ufcg.edu.br>
2025-07-07 15:49:04 -0300

    add: PrestacaoContaImpl.java

---
+++ src/main/java/webcrud/org/model/entity/dao/PrestacaoContaImpl.java
@@ -0,0 +1,40 @@
+package webcrud.org.model.entity.dao;
+
+import java.util.List;
+
+import webcrud.org.model.entity.PrestacaoConta;
+import webcrud.org.model.entity.dto.PrestacaoContaDTO;
+
+public class PrestacaoContaImpl implements PrestacaoContaDAO {
+
+	@Override
+	public List<PrestacaoContaDTO> getAllPrestacaoConta() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<PrestacaoConta> getAllPrestacaoContaDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void savePrestacaoConta() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean deletePrestacaoConta() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void updatePrestacaoConta() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
