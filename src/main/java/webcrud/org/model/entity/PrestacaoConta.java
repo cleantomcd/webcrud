61274c457085faafad93db84fa02f40059431a94
cleantomcd <cleanto.santos.macedo.neto@ccc.ufcg.edu.br>
2025-07-07 15:49:25 -0300

    add: PrestacaoConta.java

---
+++ src/main/java/webcrud/org/model/entity/PrestacaoConta.java
@@ -0,0 +1,64 @@
+package webcrud.org.model.entity;
+
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "PRESTACAO_CONTA")
+public class PrestacaoConta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String descricao;	
+	private int valor;
+	private Date data;
+	private String categoria;
+	
+	public PrestacaoConta() {}
+	
+	public PrestacaoConta(Long id, String descricao, int valor, Date data, String categoria) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+		this.valor = valor;
+		this.data = data;
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getValor() {
+		return valor;
+	}
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+}
