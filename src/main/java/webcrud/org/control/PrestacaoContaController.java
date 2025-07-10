ddd6246e283b9ec9bc3ab8702cdf8727e6102a7a
cleantomcd <cleanto.santos.macedo.neto@ccc.ufcg.edu.br>
2025-07-09 22:44:10 -0300

    feat: add data binding

--- src/main/java/webcrud/org/control/PrestacaoContaController.java
+++ src/main/java/webcrud/org/control/PrestacaoContaController.java
@@ -1,22 +1,54 @@
 package webcrud.org.control;
 
-import javax.faces.bean.ApplicationScoped;
-import javax.faces.bean.ManagedBean;
-import javax.inject.Inject;
+import java.util.List;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import webcrud.org.model.entity.PrestacaoConta;
+import webcrud.org.model.entity.dto.PrestacaoContaDTO;
 import webcrud.org.model.service.PrestacaoContaService;
 
-@ManagedBean(name = "prestacaoContaBean")
+@Named
 @ApplicationScoped
 public class PrestacaoContaController {
 
 	@Inject
 	private PrestacaoContaService prestacaoContaService;
 
+	private PrestacaoConta prestacaoSelecionada;
+
+	private List<PrestacaoConta> prestacoesSelecionadas;
 
 	public PrestacaoContaController() {
 
 	}
+
+	public void criarPrestacao() {
+		this.prestacaoSelecionada = new PrestacaoConta();
+	}
+
+	public String getMensagemBotaoDelete() {
+		if(possuiPrestacoesSelecionadas()) {
+			int size = prestacoesSelecionadas.size();
+			return size > 1 ? size + " registros selecionados" : "1 registro selecionado";
+		}
+		return "Excluir";
+	}
+
+	private boolean possuiPrestacoesSelecionadas() {
+		return prestacoesSelecionadas != null && !prestacoesSelecionadas.isEmpty();
+	}
+
+	public List<PrestacaoContaDTO> getPrestacoesCadastradas() {
+		return this.prestacaoContaService.getAllPrestacaoConta();
+	}
+
+	public PrestacaoConta getPrestacaoSelecionada() {
+		return this.prestacaoSelecionada;
+	}
+
 
 
 }
