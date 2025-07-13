4f3f8626672921e3c4a765a3719862a9ed0d3d27
elianetamara <eliane.tamara.lima.oliveira@ccc.ufcg.edu.br>
2025-07-12 21:15:00 -0300

    refactor: ajusta logica de persistencia

--- src/main/java/webcrud/org/model/service/PrestacaoContaService.java
+++ src/main/java/webcrud/org/model/service/PrestacaoContaService.java
@@ -27,38 +27,30 @@ public class PrestacaoContaService implements Serializable {
 
 	@PostConstruct
 	public void init() {
-        this.prestacoes = new ArrayList<>();
-        prestacoes.add(new PrestacaoContaDTO(10001L, "Aluguel escritório", 2500, LocalDate.of(2025, 7, 1), "1"));
-        prestacoes.add(new PrestacaoContaDTO(10002L, "Compra de materiais", 780, LocalDate.of(2025, 6, 25), "2"));
-        prestacoes.add(new PrestacaoContaDTO(10003L, "Serviços terceirizados", 1500, LocalDate.of(2025, 6, 28), "3"));
-        prestacoes.add(new PrestacaoContaDTO(10004L, "Transporte", 320, LocalDate.of(2025, 7, 5), "4"));
-
-
+		prestacaoContaImpl.savePrestacaoConta(new PrestacaoContaDTO(10001L, "Aluguel escritório", 2500, LocalDate.of(2025, 7, 1), "1"));
+		prestacaoContaImpl.savePrestacaoConta(new PrestacaoContaDTO(10002L, "Compra de materiais", 780, LocalDate.of(2025, 6, 25), "2"));
+		prestacaoContaImpl.savePrestacaoConta(new PrestacaoContaDTO(10003L, "Serviços terceirizados", 1500, LocalDate.of(2025, 6, 28), "3"));
+		prestacaoContaImpl.savePrestacaoConta(new PrestacaoContaDTO(10004L, "Transporte", 320, LocalDate.of(2025, 7, 5), "4"));
     }
 
 	public List<PrestacaoContaDTO> getAllPrestacaoConta() {
-		 //return prestacaoContaImpl.getAllPrestacaoConta(); -> apenas para mostrar o cadastro de prestacoes momentaneamente
-		return this.prestacoes;
+		 return prestacaoContaImpl.getAllPrestacaoConta();
 	}
 	public PrestacaoContaDTO getById(Long id) {
 		return prestacaoContaImpl.getById(id);
 	}
 
 	public boolean savePrestacaoConta(PrestacaoContaDTO prestacaoContaDTO) {
-		return prestacoes.add(prestacaoContaDTO); // corrigir para adicionar no repository DAO.
+		return prestacaoContaImpl.savePrestacaoConta(prestacaoContaDTO); // corrigir para adicionar no repository DAO.
 	}
 
 	public boolean deletePrestacaoConta(Long id) {
-		return prestacoes.removeIf(prestacao -> prestacao.getId().equals(id));
-		
-		//return prestacaoContaImpl.deletePrestacaoConta(id);
+		return prestacaoContaImpl.deletePrestacaoConta(id);
 	}
 	
-	public boolean deletePrestacaoConta(List<PrestacaoConta> prestacoes) {
-		return prestacoes.removeAll(prestacoes);
-		//return prestacaoContaImpl.deletePrestacaoConta(prestacoes); ---> implementar
+	public void deletePrestacaoConta(List<PrestacaoConta> prestacoes) {
+		prestacaoContaImpl.deletePrestacaoConta(prestacoes);
 	}
-
 
 	public void updatePrestacaoConta(PrestacaoContaDTO prestacaoContaDTO) {
 		prestacaoContaImpl.updatePrestacaoConta(prestacaoContaDTO);
